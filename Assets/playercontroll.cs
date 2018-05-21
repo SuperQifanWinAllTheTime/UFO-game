0b10f8ce56762081f8da8c2edfcb9297cc18a5da
SuperQifanWinAllTheTime <mr.y1994@hotmail.com>
2018-05-21 18:59:54 -0400

    finished

--- Assets/playercontroll.cs
+++ Assets/playercontroll.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playercontroll : MonoBehaviour {
 
     // Use this for initialization
     private Rigidbody2D rb2d;
+    private int count;
+    public Text counttext;
+    public Text wintext;
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        counttext.text = "Score: " + count.ToString();
+        wintext.text = "";
 	}
 	
 	// Update is called once per frame
@@ -21,4 +28,16 @@ public class playercontroll : MonoBehaviour {
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement*10);
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            counttext.text = "Score: " + count.ToString();
+            if (count>=11) {
+                wintext.text = "You Win!";
+            }
+        }
+    }
+
 }
